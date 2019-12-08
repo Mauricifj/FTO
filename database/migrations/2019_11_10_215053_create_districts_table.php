273e5bbbb5f716c5432da1d7f9d431234ee63aef
Maurici Ferreira Junior <m.f.j@hotmail.com>
2019-12-08 18:40:04 -0300

    Atualizando o controle de bairros

--- database/migrations/2019_11_10_215053_create_districts_table.php
+++ database/migrations/2019_11_10_215053_create_districts_table.php
@@ -4,33 +4,8 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-//////////////////////////////////////////////////////////////////
-//  Name:   CreateDistrictsTable - migration (class)
-//
-//  Author: Jefferson Rodrigues de Oliveira
-//
-//  Date:   27/10/2019
-//
-//  Description:
-//    Columns(Name : description)
-//      id_DIS(PK) : id for table Districts, 
-//      id_USU(FK) : user id who signed up,
-//      id_CIT(FK) : set citie for district, 
-//      name_DIS   : district name, 
-//      extra_DIS  : extra information
-//
-//  Extra specifications:
-//    When you enter a new city, the system will automatically 
-//    register adowntown district for it.
-//
-//////////////////////////////////////////////////////////////////
 class CreateDistrictsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::create('districts', function (Blueprint $table) {
@@ -40,11 +15,6 @@ class CreateDistrictsTable extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::dropIfExists('districts');
--- database/migrations/2019_11_10_235544_add_fks_districts.php
+++ database/migrations/2019_11_10_235544_add_fks_districts.php
@@ -4,26 +4,8 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-//////////////////////////////////////////////////////////////////
-//  Name:   AddFksDistricts - migration (class)
-//
-//  Author: Jefferson Rodrigues de Oliveira
-//
-//  Date:   10/11/2019
-//
-//  Description:
-//    Columns(Name : description)
-//      id_USU(FK) : user id who signed up,
-//      id_CIT(FK) : set city for district
-//
-//////////////////////////////////////////////////////////////////
 class AddFksDistricts extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::table('districts', function (Blueprint $table) {
@@ -36,11 +18,6 @@ class AddFksDistricts extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::table('districts', function (Blueprint $table) {
